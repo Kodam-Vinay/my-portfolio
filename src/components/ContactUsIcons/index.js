d41b377dc7a6745428a209e1a77cd97e135de9ee
Kodam-Vinay <vinay.kodam112@gmail.com>
2023-08-01 21:32:51 +0530

    added mail functionality

--- src/components/ContactUsIcons/index.js
+++ src/components/ContactUsIcons/index.js
@@ -1,13 +1,29 @@
 import GithubIcon from "../svgs/GithubIcon";
 import LinkedInIcon from "../svgs/LinkedInIcon";
-import DiscordIcon from "../svgs/DiscordIcon";
+import EmailIcon from "../svgs/EmailIcon";
 
 const ContactUsIcons = () => {
+  const onClickMail = () => {
+    var mail = "mailto:vinay.kodam112@gmail.com";
+    var a = document.createElement("a");
+    a.href = mail;
+    a.click();
+  };
   return (
     <>
-      <LinkedInIcon className="hover:h-10 hover:w-10 cursor-pointer" />
-      <GithubIcon className="hover:h-10 hover:w-10 cursor-pointer" />
-      <DiscordIcon className="hover:h-10 hover:w-10 cursor-pointer" />
+      <a
+        href="https://www.linkedin.com/in/vinay-kumar-kodam-4054bb21b/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <LinkedInIcon className="hover:h-10 hover:w-10 cursor-pointer" />
+      </a>
+      <a href="https://github.com/Kodam-Vinay" target="_blank" rel="noreferrer">
+        <GithubIcon className="hover:h-10 hover:w-10 cursor-pointer" />
+      </a>
+      <button type="button" onClick={onClickMail}>
+        <EmailIcon className="hover:h-10 hover:w-10 cursor-pointer" />
+      </button>
     </>
   );
 };
