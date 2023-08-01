16da2c67ecb171051ac0981b6e11f44a3eac82cb
Kodam-Vinay <vinay.kodam112@gmail.com>
2023-08-01 18:05:56 +0530

    some change

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,7 +8,7 @@ import NavigationLinks from "../NavigationLinks";
 const Header = () => {
   const [hamburgerClicked, setHamburgerClicked] = useState(false);
   return (
-    <nav className="header-container flex items-center justify-between px-4 py-2  md:px-[80px] lg:px-[170px] md:py-10 sticky top-0 h-16">
+    <nav className="header-container flex items-center justify-between px-4 py-2 md:px-[80px] lg:px-[170px] md:py-10 sticky top-0 h-16">
       <div className="header-div-container flex items-center space-x-2">
         <HomeLogo />
         <p className="font-bold text-white font-fira">Vinay</p>
