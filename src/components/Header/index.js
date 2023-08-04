241f1125545b1e822282f3f727a7aaca6ff10fa1
Kodam-Vinay <vinay.kodam112@gmail.com>
2023-08-05 00:51:11 +0530

    added navigate to home by clickng home logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,13 @@
 import HomeLogo from "../svgs/Homelogo";
 import HamburgerIcon from "../svgs/HamburgerIcon";
 import NavigationLinks from "../NavigationLinks";
-import { Link } from "react-router-dom";
+import { HashLink as Link } from "react-router-hash-link";
 
 const Header = (props) => {
   const { hamburgerClicked, setHamburgerClicked } = props;
   return (
     <nav className="header-container flex items-center justify-between px-4 py-2 md:px-[80px] lg:px-[170px] md:py-10 sticky top-0 h-16 z-10">
-      <Link className="add_zoom" to="/">
+      <Link to="#home" className="add_zoom">
         <div className="header-div-container flex items-center space-x-2 cursor-pointer">
           <HomeLogo />
           <p className="font-bold text-white font-fira">Vinay</p>
