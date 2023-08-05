081d5e196a84d9fcfbb7b77caeb6b91b9d0ef574
Kodam-Vinay <vinay.kodam112@gmail.com>
2023-08-05 23:15:03 +0530

    some css to offline page

--- src/routes/OfflinePage/index.js
+++ src/routes/OfflinePage/index.js
@@ -2,9 +2,9 @@ import OfflineLogo from "../../components/svgs/OfflineLogo";
 
 const OfflinePage = () => {
   return (
-    <div className="flex flex-col items-center justify-center m-auto">
-      <OfflineLogo className="w-96" />
-      <h1 className="text-2xl font-bold text-[#3b82f6]">
+    <div className="flex flex-col items-center justify-center h-screen p-2">
+      <OfflineLogo className="w-52 sm:w-96" />
+      <h1 className="text-xl sm:text-2xl font-bold text-[#3b82f6] text-center">
         Please! Check Your Internet Connection
       </h1>
     </div>
