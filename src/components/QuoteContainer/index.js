b24de4307e499f0cbca81be3a6ce0aca62ece4b2
Kodam-Vinay <vinay.kodam112@gmail.com>
2023-08-27 16:54:18 +0530

    changed quote

--- src/components/QuoteContainer/index.js
+++ src/components/QuoteContainer/index.js
@@ -6,7 +6,11 @@ const QuoteContainer = () => {
       <div className="flex flex-col md:justify-self-center w-[90%] md:w-[50%] relative m-auto">
         <QuoteIcon className="relative top-2 left-2 xl:left-20" />
         <div className="border m-auto flex flex-col items-center justify-center p-4 text-white font-[500] rounded-md">
-          Of course, bad code can be cleaned up. But it’s very expensive.
+          If you fail, never give up because FAIL means "First Attempt In
+          Learning.
+        </div>
+        <div className="relative w-fit border border-t-0 flex flex-col items-center justify-center xl:right-[62px] p-2 text-white font-[500] self-end rounded-b-md">
+          - DR. APJ Abdul Kalam
         </div>
       </div>
       <div className="h-14 w-14 relative border hidden top-10 md:inline-flex left-5"></div>
